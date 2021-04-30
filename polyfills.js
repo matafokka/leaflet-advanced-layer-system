703bb895a3976e4f7630a49574d8dd36976b9f11
Svyatoslav <34414488+matafokka@users.noreply.github.com>
2021-04-30 20:56:53 +0300

    Replaced project with the docs :p

--- polyfills.js
+++
@@ -1,6 +0,0 @@
-// This script contains only CoreJS and Babel polyfills.
-// These polyfills throws an error in old Chrome and prevents other polyfills from running.
-// So other polyfills has been moved to System.js.
-
-require("core-js/stable");
-require("regenerator-runtime/runtime");
